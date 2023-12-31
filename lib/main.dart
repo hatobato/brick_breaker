407b613c9af9abe9dc95f9e80ec773161b565dff
hatobato <hatobato61@gmail.com>
2023-12-31 11:10:56 +0900

    4章の実装追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'src/brick_break.dart';
+
 void main() {
-  final game = FlameGame();
+  final game = BrickBreaker();
   runApp(GameWidget(game: game));
 }
